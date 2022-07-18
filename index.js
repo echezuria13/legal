35cd7484ce757016b868ba0977c31efe81d5933f
oscar <echezuriaoscar@gmail.com>
2022-07-18 15:50:02 -0400

    Añadimos la ventana de crear usuario con Javascript

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const openMiembro = document.querySelector(".navbar__btn");
+const cerrar = document.querySelector(".close");
+const abrir = document.querySelector(".primary__btn");
+const modal = document.querySelector(".modal");
+const modalC = document.querySelector(".container__modal");
 
-openMiembro.addEventListener("click", (e) =>{
-    e.preventDefault()
-    console.log("hola")
+const inicio = document.querySelector(".navbar__button")
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault(),
+    modalC.style.opacity = "1"
+    modalC.style.visibility = "visible"
+    modal.classList.toggle("modal__close");
+});
+
+cerrar.addEventListener("click", function(e){
+    modal.classList.toggle("modal__close");
+    setTimeout(function(){
+        modalC.style.opacity = "0"
+        modalC.style.visibility = "hidden"
+    }, 1000)
 })
+
+
+window.addEventListener("click", function(e){
+//console.log(e.target)
+if(e.target === modalC){
+    modal.classList.toggle("modal__close");
+    setTimeout(function(){
+        modalC.style.opacity = "0"
+        modalC.style.visibility = "hidden"
+    }, 800)
+}
+})  
+
+
+
+inicio.addEventListener("click", function(){
+  alert("Este es mi primer sitio web. Estoy trabajando y aprendiendo con el")
+})
+
